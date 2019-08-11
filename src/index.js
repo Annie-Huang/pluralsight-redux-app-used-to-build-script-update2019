850b1ac8cf44b742fff3a0257eb492e30af001f7
Annie-Huang <annie.hn@gmail.com>
2019-08-11 19:35:55 +1000

    Create AboutPage and HomePage, Use Router to wrap the app and import bootstrap.min.css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { render } from 'react-dom';
-
+import React from "react";
+import {render} from "react-dom";
+import {BrowserRouter as Router} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 // Will get eslint error:
 // myGlobal = 4;
 
 function Hi() {
     // debugger;
-    return <p>Hi.</p>
+    return <p>Hi.</p>;
 }
 
-render(<Hi/>, document.getElementById('app'));
+render(
+    <Router>
+        <Hi/>
+    </Router>,
+    document.getElementById("app")
+);
