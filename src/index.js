928eb5af2f86ad8f15cf37040941a205082f7ec8
Annie-Huang <annie.hn@gmail.com>
2019-08-11 21:12:43 +1000

    Create App.js and index.css

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import {render} from "react-dom";
 import {BrowserRouter as Router} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App from './components/App';
+import "./index.css";
+
 // Will get eslint error:
 // myGlobal = 4;
 
-function Hi() {
-    // debugger;
-    return <p>Hi.</p>;
-}
+// function Hi() {
+//     // debugger;
+//     return <p>Hi.</p>;
+// }
+
 
 render(
     <Router>
-        <Hi/>
+        <App />
     </Router>,
     document.getElementById("app")
 );
