5eec7235ecec73461b4b37f915f43fb75ce1e95a
Annie-Huang <annie.hn@gmail.com>
2019-08-15 21:59:53 +1000

    Improve performance by stopping calls to load courses and authors unless the lists are emtpy.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -8,13 +8,18 @@ import CourseList from "./CourseList";
 
 class CoursesPage extends Component {
     componentDidMount() {
-        this.props.actions.loadCourses().catch(error => {
-            alert("Loading courses failed" + error);
-        });
+        const { courses, authors, actions } = this.props;
+        if (courses.length === 0) {
+            actions.loadCourses().catch(error => {
+                alert("Loading courses failed" + error);
+            });
+        }
 
-        this.props.actions.loadAuthors().catch(error => {
-            alert("Loading authors failed" + error);
-        });
+        if (authors.length === 0) {
+            actions.loadAuthors().catch(error => {
+                alert("Loading authors failed" + error);
+            });
+        }
     }
 
     render() {
@@ -28,6 +33,7 @@ class CoursesPage extends Component {
 }
 
 CoursesPage.propTypes = {
+    authors: PropTypes.array.isRequired,
     courses: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired
 };
