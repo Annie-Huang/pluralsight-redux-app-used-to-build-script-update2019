7c3a6649882dfe9b624e58bc93a4ed19144e2990
Annie-Huang <annie.hn@gmail.com>
2019-08-15 21:36:16 +1000

    Create authorActions.js. Create authorReducers.js. Add authorReducer into rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 // The reason we can call "course" (from import 'courses') in below is because
 // the courseReducer.js only got one "export default"
 import courses from "./courseReducer";
+import authors from "./authorReducer";
 
 const rootReducer = combineReducers({
-    courses
+    courses,
+    authors
 });
 
 export default rootReducer;
