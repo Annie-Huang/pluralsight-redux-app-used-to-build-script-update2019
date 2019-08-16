4e00763e56d8e5d319bb7bbdf481c9f9876ceba5
Annie-Huang <annie.hn@gmail.com>
2019-08-16 20:04:59 +1000

    Add route to ManageCoursePage

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
 import CoursesPage from "./courses/CoursesPage";
+import ManageCoursePage from "./courses/ManageCoursePage";
 
 const App = () => {
     return (
@@ -14,6 +15,9 @@ const App = () => {
                 <Route exact path="/" component={HomePage} /> {/*React Router will watch the URL and render the proper route. Our Header will always display above*/}
                 <Route path="/about" component={AboutPage} />
                 <Route path="/courses" component={CoursesPage} />
+                {/*The slug is a url friendly id. It's more readable. Since only one route in Switch can match, we need to declare this more specific route frist*/}
+                <Route path="/course/:slug" component={ManageCoursePage} />
+                <Route path="/course" component={ManageCoursePage} />
                 <Route component={PageNotFound} />
             </Switch>
         </div>
