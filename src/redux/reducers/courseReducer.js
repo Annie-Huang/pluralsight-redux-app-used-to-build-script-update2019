7e7bb1352b3edaa3bf4fe79f426247aaca2ac010
Annie-Huang <annie.hn@gmail.com>
2019-08-16 21:53:01 +1000

    Update courseReducer.js to handle CREATE_COURSE_SUCCESS and UPDATE_COURSE_SUCCESS

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -4,8 +4,15 @@ import initialState from "./initialState";
 // export default function courseReducer(state = [], action) {
 export default function courseReducer(state = initialState.courses, action) {
     switch (action.type) {
-        case types.CREATE_COURSE:
+        // case types.CREATE_COURSE:
+        case types.CREATE_COURSE_SUCCESS:
             return [...state, { ...action.course }];
+        case types.UPDATE_COURSE_SUCCESS:
+            return state.map(course =>
+                // map returns a new array. I'm replacing the element with the matching course.id
+                // With Redux, you'll often use the spread operator, map, filter, and reduce.
+                course.id === action.course.id ? action.course : course
+            );
         case types.LOAD_COURSES_SUCCESS:
             return action.courses;
         default:
