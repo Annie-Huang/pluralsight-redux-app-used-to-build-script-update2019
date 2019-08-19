87ecb9d7188dbd8be4781c3aa81c299c351376df
Annie-Huang <annie.hn@gmail.com>
2019-08-19 21:43:52 +1000

    Make sure we call dispatch(apiCallError(error)); when we failed to loadCourses and loadAuthors as well.

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -27,6 +27,7 @@ export function loadCourses() {
                 dispatch(loadCoursesSuccess(courses));
             })
             .catch(error => {
+                dispatch(apiCallError(error));
                 throw error;
             });
     };
